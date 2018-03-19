f69c61674ba60504f6934ba59a8c4ed20dcf0136
Samat Baltin <somych1@gmail.com>
2018-03-19 16:42:25 -0500

    Commit 2 - It's all about Me done

--- SamatBaltin/app.js
+++ SamatBaltin/app.js
@@ -4,3 +4,23 @@ const clicker = {
 		this.clickCount += 1;
 	}
 }
+
+const myInformation = {
+	name: "Samat",
+	age: 28,
+	facts: ['male', 'student'],
+	statement(){
+		console.log(this.name);
+		console.log(this.facts);
+		console.log(this.age += 10);
+	},
+	ageIncr(){
+		this.age += 1;
+		return this.age
+	},
+	nameChange(name){
+		this.name = name;
+		return this.name
+	}
+}
+myInformation.nameChange('Rumpelstiltskin');

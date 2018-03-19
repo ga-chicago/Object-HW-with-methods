b7e35bd06f041388e56b2eb09ebc9846e799c90a
Tyler Earls <tyler.a.earls@gmail.com>
2018-03-19 16:37:29 -0500

    Commit 2 - It's all about Me done

--- js/app.js
+++ js/app.js
@@ -9,3 +9,25 @@ const clicker = {
 };
 
 console.log(clicker.click());
+
+// IT'S ALL ABOUT ME
+
+const myInformation = {
+	name: "Tyler Earls",
+	age: 24,
+	fact: "Star Trek fan",
+	statement () {
+		return ("My name is " + this.name + ", I will be " + (this.age + 10) + " years old in 10 years, and something interesting about me is I am a " + this.fact + ".");
+	},
+	birthday () {
+		this.age += 1;
+	},
+	changeName () {
+		this.name = "Rumpelstiltskin";
+	}
+}
+console.log(myInformation.statement());
+myInformation.birthday();
+console.log(myInformation.age);
+myInformation.changeName();
+console.log(myInformation.name);

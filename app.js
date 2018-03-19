4a8198485c63ec001964ec2038eee6ace918dca6
Noemi Romero <nlll02@yahoo.com>
2018-03-19 18:19:28 -0500

    Commit 1 - Clicker done

---
+++ app.js
@@ -0,0 +1,52 @@
+console.log("hello")
+
+//#1.
+
+const clicker = {
+	clickCount: 0,
+	click() {
+		this.clickCount += 1;
+		return this.clickCount;
+		console.log("this.clickCount")
+	}
+}
+// hello
+// clicker
+// {clickCount: 0, click: ƒ}
+// clicker.clickCount
+// 0
+// clicker.click()
+// 1
+
+//////////////
+//retype if it doesn't work 
+
+/////////////////
+const myInformation = {
+	statement () {
+		return {
+			name: "Noemi",
+ 			age: 31,
+ 			fact: "I am not a quiter."
+
+		}
+	}
+}
+myInformation.statement()
+
+
+
+
+
+////////////////
+
+
+
+
+
+
+
+
+
+
+

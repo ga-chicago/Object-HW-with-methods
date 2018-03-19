982ad9892dd3b0becd6891560e448b6e977f689c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-19 16:35:03 -0500

    Initial Commit- Clicker Done

---
+++ JS/app.js
@@ -0,0 +1,21 @@
+//Clicker
+
+
+const clicker = {
+	clickCount:0,
+
+	click(){
+		this.clickCount += 1;
+	}
+}
+
+
+clicker.click();
+clicker.click();
+clicker.click();
+
+console.log(clicker.clickCount);
+
+
+
+//It's All About Me

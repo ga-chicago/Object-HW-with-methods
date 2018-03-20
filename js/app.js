b56e49bfee568a83a70bc289cc410674932c6f84
Zoe <zoereagan@gmail.com>
2018-03-19 21:36:52 -0500

    commit 1 - clicker done

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//Homework 19 March 2018
+
+//1. Clicker
+//Make an object called clicker, which has a clickCount property,
+//and a click method.
+
+const clicker = {
+  clickCount: 0,
+  click() {
+//Have the click method increment the clickCount by 1 each time
+//it is called.
+      return this.clickCount += 1;
+  }
+}
+
+//2. Its All About Me
+const myInformation = {
+  name: "Zoe",
+  age: 26,
+  fact: "I've never had a cavity.",
+  getOlder () {
+            this.age += 1;
+            return this.age;
+  },
+  newName () {
+          this.name = "Rumplestiltskin"
+          console.log(this.name);
+  }
+}
+
+myInformation.getOlder();
+myInformation.newName()
